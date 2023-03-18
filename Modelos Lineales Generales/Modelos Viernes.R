69d799e388dd65626cf31790a47329c43660fd7b
Andrés Felipe Palomino Montezuma <palomino.andres@correounivalle.edu.co>
2023-03-18 09:15:01 -0500

    Update Modelos Viernes.R

--- Modelos Lineales Generales/Modelos Viernes.R
+++ Modelos Lineales Generales/Modelos Viernes.R
@@ -3,12 +3,13 @@
 ######### Andrés Felipe Palomino Monteuma - Escuela de estadística#####
 ####### Universidad del Valle - Modelo Lineal General I################
 #Visualización datos catégoricos
-set.seed(10)
+set.seed(10) #Semilla para que los resultados sean replicables
 x<- sample(c('Excelente','Regular','Malo'),size=10,replace=T,prob=c(0.2,0.4,0.4))
 y<- sample(c('Excelente','Regular','Malo'),size=10,replace=T,prob=c(0.8,0.1,0.1))
 z<- sample(c('Excelente','Regular','Malo'),size=10,replace=T,prob=c(0.9,0.05,0.05))
 X<- as.data.frame(cbind(x,y,z))
 colnames(X)<- c('Per.Clase','Per.Monitoria','Per.Calif')
+View(X)
 tablas<- apply(X,2,table)
 for( i  in 1:ncol(X)){
   barplot(tablas[[i]],las=2,panel.first=grid(),main=colnames(X)[i],col=c("aquamarine","aquamarine4","red1")) 
@@ -27,37 +28,42 @@ pie(tablas[[i]], labels = rotulos,col=c("aquamarine1","red1","brown","purple"),l
 #Diagramas de Caja
 X<- as.data.frame(cbind(rnorm(50,10,2),rpois(50,10),rweibull(50,2,6)))
 colnames(X)<- c('Indíce','Conteos','Tiempos')
-par(mar=c(3,2,2,2))
-par(mfrow=c(2,2))
+par(mar=c(2,2,2,2))
+par(mfrow=c(1,3))
 sapply(seq(1,3),function(j)boxplot(X[,j],main=colnames(X)[j],xlab="",col="blue"))
-X$Grupos<- c(rep(1,20),rep(2,20),rep(3,10))
+X$Grupos<- as.factor(c(rep(1,20),rep(2,20),rep(3,10)))
+View(X)
 par(mfrow=c(1,3))
 colnames(X)
 sapply(seq(1,3),function(j)boxplot(X[,j]~X[,4],main=colnames(X)[j],xlab="",col="blue"))
 ###########################################################################
-par(mfrow=c(2,2))
+par(mfrow=c(1,3))
 par(mar=c(4,2,4,4))
-sapply(seq(1,3),function(j)hist(X[,j],main=colnames(X)[j],xlab="",col="khaki4",breaks="Sturges"))
+sapply(seq(1,3),function(j)hist(X[,j],main=colnames(X)[j],xlab="",col="khaki4",xlim=c(0,25),breaks="Sturges"))
 #####################
 # Densidades Kernell
-par(mfrow=c(2,2))
-sapply(seq(1,4),function(j){
+par(mfrow=c(1,3))
+sapply(seq(1,3),function(j){
   plot(density(X[,j],kernel="gaussian"),main=colnames(X)[j],xlab="",col="brown",lwd=2)
   lines(density(X[,j],kernel="epanechnikov"),main=colnames(X)[j],xlab="",col="cadetblue",lwd=2)
   lines(density(X[,j],kernel="triangular"),main=colnames(X)[j],xlab="",col="firebrick",lwd=2)
   lines(density(X[,j],kernel="cosine"),main=colnames(X)[j],xlab="",col="black",lwd=2)}
 )
+class(X[,1])
 ################
 X<- as.data.frame(cbind(rnorm(50,10,2),rpois(50,10),rweibull(50,2,6)))
 View(X)
 colnames(X)<- c('Indíce','Conteos','Tiempos')
-y<- 2+5*(1:100)+rnorm(100,50,100)
+y<- 2+5*(1:100)+rnorm(100,50,50)
 x<- 1:100
-plot(x,y,panel.first=grid(),pch=19,col=c('aquamarine3','red1','purple'),xlab='')
+par(mfrow=c(1,1))
+par(mar=c(4,4,4,4))
+plot(x,y)
+plot(x,y,panel.first=grid(),pch=19,col=c('aquamarine3','red1','purple'),xlab='Porcentaje de Admitidos',ylab=' Número de inscritos',main='Diagrama de dispersión')
 #Formato latéx
 #main = expression(alpha[1] ^ 2 + frac(beta, 3))
 #Texto dentro de los gráficos
-plot(x, y, main = "Título principal", cex = 2, col = "blue",panel.first = grid())
+plot(x, y, main = "Título principal", cex = 1, col = "blue",panel.first = grid())
 
 #---------------
 # Función mtext
@@ -91,10 +97,10 @@ mtext("Texto arriba", side = 3, line = 2.5)
 #--------------
 
 # Texto en las coordenadas (-2, 2)
-text(-2, 2, "Más texto")
+text(20, 100, "Más texto")
 
 # Fórmula en las coordenadas (3, -3)
-text(3, -3, expression(frac(alpha[1], 4)))
+text(20, 200, expression(frac(alpha[1], 4)))
 ############## Texto
 # Fuentes
 
@@ -117,8 +123,9 @@ plot(x, y, main = "Mi título", sub = "Mi subtítulo",
      cex.lab = 3,    # Tamaño de las etiquetas de los ejes X e Y
      cex.axis = 0.5) # Tamaño de las etiquetas de los ticks de los ejes
 library(latex2exp)
-plot(x, y, main = TeX('$\\beta^3, \\beta_{1} ,\\theta_{10}$'))
+plot(x, y, main = TeX('$\\beta^3, \\mu_{1} ,\\lambda_{10}$'))
 ############# Función de Resumen 
+summary(X)
 resumen<- function(x){
   X<- matrix(0,9,1)
   resumen<- round(c(mean(x),median(x),min(x),max(x),var(x),sd(x),quantile(x,0.25),quantile(x,0.75),sd(x)/mean(x)),4)
@@ -132,6 +139,7 @@ resumen<- function(x){
 resumen(x)
 library(xtable)
 xtable(resumen(x))
+View(X)
 ######################## Introducción a la imputación
 X[1,3]<- NA
 X[10,4]<- NA
@@ -142,4 +150,32 @@ for(i in 1:ncol(X)){
   X[,i]<-ifelse(is.na(X[,i]),sample(X[,i],1),X[,i])
 }
 View(X)
-
+#######################################################
+############################# Clase modelos lineales#######################
+library(alr4) ###### Importación de la librería############################
+data("UBSprices") ###### Base de datos#####################################
+?UBSprices
+View(UBSprices) # Visualización de base datos
+X<- UBSprices #Guardo la matriz 
+####################
+pairs(UBSprices) #Exploración inicial de las variables
+plot(UBSprices$rice2003,UBSprices$rice2009)######### Gráfico de dispersión de mi variable de interés
+##########
+summary(X)# Resumen de la matriz, con estadísticos 
+##############Variable auxiliar para ver el cambio en los precios
+diferencia<- as.data.frame(X$rice2009-X$rice2003)
+X$diferencia<- diferencia
+names(X)
+rownames(diferencia)<- rownames(X)
+max(diferencia[diferencia>0]) 
+min(diferencia[diferencia<0])
+########### Coeficiente de correlación lineal de pearson
+cor(UBSprices$rice2003,UBSprices$rice2009)
+######################### #Gráficon con la variable auxiliar
+plot(UBSprices$rice2003,UBSprices$rice2009,xlab="Precio del arroz en minutos de trabajo en el año 2003",ylab="Precio del arroz en minutos de trabajo en el año 2009")
+points(UBSprices$rice2003[diferencia>0],UBSprices$rice2009[diferencia>0],col="blue4",pch=19)
+points(UBSprices$rice2003[diferencia<0],UBSprices$rice2009[diferencia<0],col="red4",pch=19)
+points(UBSprices$rice2003[diferencia==0],UBSprices$rice2009[diferencia==0],col="black",pch=19)
+legend(x = "bottomright",legend=c("Aumento","Disminuyo","Se mantuvo"),
+       col = c("blue4","red4","black"),pt.cex=1,pch=c(19,19,19),
+       box.lwd=0.6,text.font =15,cex=0.8)
