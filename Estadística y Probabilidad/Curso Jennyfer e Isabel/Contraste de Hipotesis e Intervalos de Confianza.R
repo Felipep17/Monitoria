a400dc9da078fd6bc29900a460c703949d495a85
Andrés Felipe Palomino Montezuma <palomino.andres@correounivalle.edu.co>
2023-09-11 09:37:28 -0500

    Actualizando

--- Estadística y Probabilidad/Curso Jennyfer e Isabel/Contraste de Hipotesis e Intervalos de Confianza.R
+++ Estadística y Probabilidad/Curso Jennyfer e Isabel/Contraste de Hipotesis e Intervalos de Confianza.R
@@ -1,26 +1,40 @@
 #### Contraste de Hipotésis e Intervalos de Confianza ####
 set.seed(1)
 library(BSDA)
-p1<- rnorm(100,50,1)
-p2<- rnorm(100,10,1)
+p1<- rnorm(100,5,1)
+p2<- rnorm(100,3,1)
 X<-data.frame(cbind(c(p1,p2),c(rep(1,100),rep(2,100))))
 colnames(X)<- c("Var","Group")
 boxplot(X$Var~X$Group,xlab="Grupo",ylab="Variable")
 boxplot(rnorm(100,0,1))
 p0<- rnorm(1000,10,1)
 hist(p0)
-sd(p0)
+sd(p1)
 sd(p2)
+options(scipen=999)
 z.test(
   x=p1,
-  y = p2,
-  alternative = "two.sided",  mu = 0,  sigma.x = 1.05717,  sigma.y = 1.038411,  conf.level = 0.95)
+  y = NULL,
+  alternative = "greater",  mu = 3,  sigma.x = 1.080836,  sigma.y = NULL,  conf.level = 0.95)
 t.test(
   x           =p0 ,
-  y           = NULL,
+  y           = p1,
   alternative = "two.sided",
   mu          = 0,
-  var.equal   = TRUE,
+  var.equal   = T,
   conf.level  = 0.95
 )
+#### Mejoremos los gráficos ####
+boxplot(X$Var~X$Group,main="",xlab="Grupo",ylab="Variable",pch=19,col="white",border=c("blue4","pink"))
+grid(nx = NULL, ny = NULL,
+     lty = 1, col = "gray", lwd = 1)
+boxplot(X$Var~X$Group,xlab="Grupo",ylab="Varible",pch=19,col="white",border=c("blue4","pink"),add=T)
+# Points
+stripchart(X$Var~X$Group,              # Data
+           method = "jitter", # Random noise
+           pch = 19,          # Pch symbols
+           col = c("blue4","pink"),           # Color of the symbol
+           vertical = TRUE,   # Vertical mode
+           add = TRUE)   
+
                                     
